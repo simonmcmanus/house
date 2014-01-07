9bf40b8c6f65051823a057181b37823c847c5982
Simon McManus <mcmanus.simon@gmail.com>
2014-01-07 17:47:23 +0000

    passwod

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(function *(next){
 console.log('username is', process.env.HOUSEUSER);
 
 
-app.use(auth({ name: process.env.HOUSEUSER, pass: process.env.HOUSEPASS }));
+app.use(auth({ name: process.env.HOUSEUSER, pass: process.env.HOUSEPASSWORD}));
 
 app.use(route.get('/', function *(){
   console.log('her');
